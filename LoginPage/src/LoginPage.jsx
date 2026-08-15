709c5593321b93c757c3e11e6a07937967afcc00
Tarun Singh <singhnikitarun@gmail.com>
2026-08-15 17:37:07 +0530

    Styling in Login page

--- LoginPage/src/LoginPage.jsx
+++ LoginPage/src/LoginPage.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
+import './loginPage.css'
 
 function LoginPage() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <div>
-        Sign In/Sign Up
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
-      </div>
-      <div>
-        <button>Sign Up</button>
+    <div className='body'>
+      <div className='mainContainer'>
+        <div className='container'>
+          <h1>
+            Login
+          </h1>
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <div className='inputField'>
+              <input type="text" placeholder="Username" />
+              <span>icon</span>
+            </div>
+            <div className='inputField'>
+              <input type="password" placeholder="Password" />
+              <span>icon</span>
+            </div>
+            <div className='forgotSection'>
+              <input type="checkbox" /> Remember Me
+              <button className='forgotButton'>Forgot Password?</button>
+            </div>
+
+            <button className='loginButton'>Login</button>
+          </div>
+          <div className='registerSection'>
+            <span>
+              Don't have an account?
+            </span>
+            <button className="registerButton">Register</button>
+          </div>
+        </div>
       </div>
     </div>
   )
