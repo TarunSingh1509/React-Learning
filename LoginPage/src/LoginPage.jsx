f05e49d05589ce38b550b6e6e24beb52ab9cad29
Tarun Singh <singhnikitarun@gmail.com>
2026-08-18 17:30:34 +0530

    Fixing some issues in Login Page

--- LoginPage/src/LoginPage.jsx
+++ LoginPage/src/LoginPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './loginPage.css'
 import { FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 function LoginPage() {
+  const [passwordShow,setPasswordShow]=useState(false);
+  const showPassword=()=>{
+    ;
+  }
+
   return (
     <div className='body'>
-      <div className='mainContainer'>
         <div className='container'>
           <h1>
             Login
@@ -17,14 +21,13 @@ function LoginPage() {
               <span style={{cursor:"pointer"}}><MdEmail /></span>
             </div>
             <div className='inputField'>
-              <input type="password" placeholder="Password" />
-              <span style={{cursor:"pointer"}}><FaLock /></span>
+              <input type={passwordShow?"text":"password"} placeholder="Password" />
+              <span style={{cursor:"pointer"}} onClick={()=>setPasswordShow(!passwordShow)} ><FaLock /></span>
             </div>
             <div className='forgotSection'>
               <input type="checkbox" /> Remember Me
               <button className='forgotButton'>Forgot Password?</button>
             </div>
-
             <button className='loginButton'>Login</button>
           </div>
           <div className='registerSection'>
@@ -35,7 +38,6 @@ function LoginPage() {
           </div>
         </div>
       </div>
-    </div>
   )
 }
 
